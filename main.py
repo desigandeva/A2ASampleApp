114621e6e74d2d922d425d0e5af2bb2d9512d9b5
rmdesigan0303 <rmdesigan0303@gmail.com>
2026-02-13 08:08:25 +0000

    First commit

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+@app.route("/.well-known/agent.json", methods=["GET"])
+def agent_card():
+  """."""
+  return jsonify({
+    "name": "Mailer Agent",
+    "description": "Draft and Send a Mail",
+    "url": "http://127.0.0.1:3000",
+    "version": "1.0",
+    "capabilites": {
+      "streaming": False,
+      "pushNotifications": False
+    }
+  })
+
+
+@app.route("/task/send", methods=["POST"])
+def handle_tasl():
+  try:
+    print("code",request.get_json())    
+  except Exception as e:
+    print(e)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
